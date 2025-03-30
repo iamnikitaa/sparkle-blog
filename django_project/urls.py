151d27058dfd14c5008064183d15b3612c4f682a
Nikita Kumari <iamnikita004@gmail.com>
2025-03-30 13:11:01 +0530

    Enhance About Page content and styling, update profile image handling, and improve CSS styles

--- django_project/urls.py
+++ django_project/urls.py
@@ -23,6 +23,8 @@ from users import views as user_views
 from django.conf import settings
 from django.conf.urls.static import static
 
+
+
 # Debugging Print
 print("Including blog.urls!")
 
--- users/models.py
+++ users/models.py
@@ -4,14 +4,13 @@ from PIL import Image  # Ensure this is imported
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
-
+    image = models.ImageField(default='profile_pics/default.jpg', upload_to='profile_pics')
     def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)  # Save the profile first
+        super().save(*args, **kwargs) 
 
-        if self.image and self.image.name != "default.jpg":  # Avoid processing default image
+        if self.image and self.image.name != "default.jpg":  
             img = Image.open(self.image.path)
             if img.height > 300 or img.width > 300:
                 output_size = (300, 300)
                 img.thumbnail(output_size)
-                img.save(self.image.path)  # Resize and save the image
+                img.save(self.image.path) 
